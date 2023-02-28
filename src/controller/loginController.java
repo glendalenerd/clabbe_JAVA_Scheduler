fd2979e390f20379c861cf233a6fc62500c06ba7
Chris Labbe <clabbe12@yahoo.com>
2023-02-27 17:09:20 -0800

    Adding imports to login controller

---
+++ src/controller/loginController.java
@@ -0,0 +1,25 @@
+package src.controller;
+import src.database.JDBC;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Controller for login menu
+ * @author Chris Labbe
+ */
+public class loginController {
+
+}
