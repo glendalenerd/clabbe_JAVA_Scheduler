b0f5f685ff6188cc041c86f58bf99cb567e78b9f
Chris Labbe <clabbe12@yahoo.com>
2023-03-12 20:53:42 -0700

    Added table and column data to Client screen

--- src/controller/clientsController.java
+++ src/controller/clientsController.java
@@ -5,13 +5,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import src.database.clientQueries;
 import src.model.clientModel;
 import src.utilities.utilityFunctions;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class clientsController {
+public class clientsController implements Initializable {
     private final ObservableList<clientModel> allClients = FXCollections.observableArrayList();
     @FXML
     public TableView<clientModel> clientTable;
@@ -53,6 +59,24 @@ public class clientsController {
     public Button editButton;
     @FXML
     public Button backButton;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            allClients.setAll(clientQueries.getClientList());
+        } catch (SQLException i) {
+            throw new RuntimeException(i);
+        }
+    clientTable.setItems(allClients);
+    clientTable.refresh();
+    clientIdColumn.setCellValueFactory(new PropertyValueFactory<>("clientId"));
+    clientNameColumn.setCellValueFactory(new PropertyValueFactory<>("clientName"));
+    clientEmailColumn.setCellValueFactory(new PropertyValueFactory<>("clientEmail"));
+    hairColorColumn.setCellValueFactory(new PropertyValueFactory<>("clientPrefHairColor"));
+    activeStatusColumn.setCellValueFactory(new PropertyValueFactory<>("activeStatus"));
+    stateProvColumn.setCellValueFactory(new PropertyValueFactory<>("clientStateProv"));
+    clientCountryColumn.setCellValueFactory(new PropertyValueFactory<>("clientCountry"));
+    clientPostalCodeColumn.setCellValueFactory(new PropertyValueFactory<>("clientZipCode"));
+    }
     public void menuReturn(ActionEvent click) throws IOException {
         utilityFunctions.menuOpen(click, "../view/menu.fxml");
     }
